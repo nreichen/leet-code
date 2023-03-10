6145119998c2c9f8f135abd3ee74bcac183bf6b0
Nathan <55863770+nreichen@users.noreply.github.com>
2023-03-10 18:48:12 -0500

    Time: 118 ms (5.27%), Space: 17 MB (56.81%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int answer = 0;
+        for (auto num: nums) {
+            answer ^= num;
+            cout << answer << " ";
+        }
+        return answer;
+    }
+};
