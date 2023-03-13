9fb3d608c3728d3a6156e5cbdd6afef213933c10
Nathan <55863770+nreichen@users.noreply.github.com>
2023-03-13 17:03:02 -0400

    Time: 0 ms (100.00%), Space: 5.8 MB (99.85%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -4,7 +4,8 @@ public:
         int rev = 0;
         while (x != 0) {
             int digit = x % 10;
-            if ((rev > 0 && rev > (INT_MAX - digit)/10) | (rev < 0 && rev < (INT_MIN - digit)/10)) return 0;
+            if ((rev > 0 && rev > (INT_MAX - digit)/10) |
+                (rev < 0 && rev < (INT_MIN - digit)/10)) return 0;
             rev = rev * 10 + digit;
             x = x / 10;
         }
